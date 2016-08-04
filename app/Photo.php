2f35cca44d3381559baea305b76e033f0d436b39
Tiendeptrai <tiendeptrai@yahoo.com>
2016-08-04 16:26:37 +0700

    users with uploading feature working

--- app/Photo.php
+++ app/Photo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+
+
+    protected $fillable = ['file'];
+
+
 }
--- app/User.php
+++ app/User.php
@@ -30,8 +30,9 @@ class User extends Authenticatable
 
 
 
-
-
+    public function photo(){
+        return $this->belongsTo('App\Photo');
+    }
 
 
 
